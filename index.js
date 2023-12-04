0a45efd1c855efadbbff978b796eeb3b3e4b9799
minaj27 <minaj13m@gmail.com>
2023-12-04 23:54:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function handleAdd(w,h,clr){
+    document.getElementById('show').style.width=w+'px'
+    document.getElementById('show').style.height=h+'px'
+    document.getElementById('show').style.backgroundColor=clr
+}
